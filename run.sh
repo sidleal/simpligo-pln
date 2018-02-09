e689353c3b6f315c8ed3136814e991bf8999f6b6
Sidney Leal <sidleal@gmail.com>
2018-02-09 12:16:04 -0200

    run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker stop simpligo-pln && docker rm simpligo-pln && docker run -d --name simpligo-pln --add-host elasticsearch:172.17.0.1 --restart always simpligo-pln:$1
+docker stop simpligo-pln && docker rm simpligo-pln && docker run -d --name simpligo-pln -p 80:8080 --add-host elasticsearch:172.17.0.1 --restart always simpligo-pln:$1
