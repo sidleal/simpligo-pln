5b4f39ac3323b0d3f148d8d46ebfad65a792c963
Sidney Leal <sidleal@gmail.com>
2018-12-19 17:35:39 -0200

    timeout

--- static/js/ranker.js
+++ static/js/ranker.js
@@ -3,6 +3,7 @@ function eval() {
     $.ajax({
         type: 'POST',
         url: '/ranker/eval',
+        timeout: 30000,
         headers: {
             "Authorization": sessionStorage.getItem('simpligo.pln.jwt.key')
         },
@@ -12,9 +13,10 @@ function eval() {
     }).done(function(data) {
         $('#results').show();
         $('#output').val(data);        
-    }).fail(function(error) {
+    }).fail(function(xhr, status, error) {
+        console.log(xhr.responseText);
         console.log(error);
-        alert( "Erro" );
+        alert(xhr.responseText);
     });
 
   }
