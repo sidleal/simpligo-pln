ac6731a0a6b1984470c13a544ad4cf0661645b9d
Sidney Leal <sidleal@gmail.com>
2020-07-04 16:01:43 -0300

    app-runner

--- tools/app-runner/app-runner.go
+++ tools/app-runner/app-runner.go
@@ -23,6 +23,7 @@ func Router() *mux.Router {
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	r.HandleFunc("/ranker", RankerHandler).Methods("POST")
+	r.HandleFunc("/ranking", RankingHandler).Methods("POST")
 	r.HandleFunc("/metrics/{subset}", MetricsHandler).Methods("POST")
 
 	return r
@@ -188,4 +189,97 @@ func MetricsHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(out)
+}
+
+func RankingHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text")
+
+	ret := ""
+
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		ret += "Error reading req: %v." + err.Error()
+		log.Println(ret)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, ret)
+		return
+	}
+
+	text := string(body)
+	text = strings.Replace(text, "\"", "'", -1)
+	text = strings.Replace(text, "!", "! ", -1)
+	ret += text + "\n"
+	ret += "-------------------------------" + "\n"
+
+	// log.Println(text)
+	log.Println("/bin/bash", "-c", "coh-metrix-nilc/run_sentence156.sh \""+text+"\"")
+
+	cmd := exec.Command("/bin/bash", "-c", "coh-metrix-nilc/run_sentence156.sh \""+text+"\"")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		ret += "cmd.Run() failed with " + err.Error()
+		log.Println(ret)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, ret)
+		return
+	}
+	fmt.Printf("combined out:\n%s\n", string(out))
+
+	features := ""
+
+	regEx := regexp.MustCompile(`([^\[]*)\[(.*)\]`)
+	match := regEx.FindStringSubmatch(string(out))
+	if len(match) < 1 {
+		ret += "Erro resposta cohmetrix: \n" + string(out)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, ret)
+		return
+	}
+
+	log.Println(match[2])
+	features = match[2]
+
+	ret += match[1] + "\n"
+	ret += "-------------------------------" + "\n"
+
+	log.Println("/bin/bash", "-c", "simpligo-ranking/run.sh "+features)
+
+	cmd2 := exec.Command("/bin/bash", "-c", "simpligo-ranking/run.sh "+features)
+	out2, err := cmd2.CombinedOutput()
+	if err != nil {
+		ret += "cmd.Run() failed with " + err.Error()
+		log.Println(ret)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, ret)
+		return
+	}
+	fmt.Printf("combined out:\n%s\n", string(out2))
+
+	complexity := ""
+
+	regEx2 := regexp.MustCompile(`result: ([0-9\.]+)`)
+	match2 := regEx2.FindStringSubmatch(string(out2))
+	if len(match2) > 0 {
+		log.Println(match2[1])
+		ret += match2[1] + "\n"
+
+		floatVal, _ := strconv.ParseFloat(match2[1], 32)
+		complexity = fmt.Sprintf("%.2f", floatVal*100)
+
+	}
+
+	ret += "-------------------------------" + "\n"
+
+	w.WriteHeader(http.StatusOK)
+
+	result := ""
+	result += "====================================\n"
+	result += "Complexidade --> " + complexity + "\n"
+	result += "====================================\n"
+	result += "\n\n" + ret
+
+	w.Write([]byte(result))
+	// fmt.Fprint(w, ret)
 }
